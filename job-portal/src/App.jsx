3f8b5826ea40c55efe11d4d18b6ab1f21951e18e
maureen <maureen.amech123@gmail.com>
2025-07-23 18:36:26 +0100

    Starter Files

---
+++ job-portal/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>  
+     <div className='my-app'>This is my Practice text</div>    
+     </>
+  )
+}
+
+export default App
